cea2ed77097e270df6e018698abb3e10da1e1502
Mehedimid <mehedihasan25276@gmail.com>
2023-09-17 23:02:08 +0600

    added cart section and designed it

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import Cart from "./Cart";
 
 function Home() {
   const [data, setData] = useState([]);
+  const [selectedCard , setCard] = useState([])
+  
 
   useEffect(() => {
     fetch("./data.json")
@@ -10,14 +13,21 @@ function Home() {
       .then((data) => setData(data));
   }, []);
 
+ const  handleSelectCourse = card => {
+
+    
+    setCard([...selectedCard, card])
+ } 
+ console.log(selectedCard)
+
   return (
     <>
       <h1 className="text-4xl text-center my-6 font-semibold">
         Course Registration
       </h1>
-      <div className="max-w-6xl mx-auto flex gap-5">
+      <div className="max-w-6xl mx-auto flex gap-5 mb-12">
         {/* card single */}
-        <div className="border w-[75%] grid-cols-3 grid gap-5">
+        <div className=" w-[75%] grid-cols-3 grid gap-5">
           {data.map((card) => (
             <div className="card w-full  text-primary-content  shadow-xl border-red-300">
               <div className="card space-y-3">
@@ -33,7 +43,7 @@ function Home() {
                   <p>Credit: {card.credit} hr</p>
                 </div>
                 <div className="card-actions justify-center">
-                  <button className="btn btn-error mb-3">Select</button>
+                  <button onClick={()=>handleSelectCourse(card)} className="btn btn-error mb-3">Select</button>
                 </div>
               </div>
             </div>
@@ -41,8 +51,8 @@ function Home() {
         </div>
 
         {/* cart here  */}
-        <div className="border">
-          <h1 className="5xl"> hello there</h1>
+        <div className="shadow-xl h-fit  rounded-xl p-2 w-[25%] bg-orange-50">
+          <Cart selectedCard={selectedCard} key={selectedCard.id}></Cart>
         </div>
       </div>
     </>
